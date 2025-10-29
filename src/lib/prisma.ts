323a58a242fdab0ecc0754b394f8348dfcb8aeb3
diego_mur <diegomurcia2@gmail.com>
2025-10-29 00:08:31 -0600

    chore: remove prisma logic, file now deprecated after DynamoDB migration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,1 @@
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-// Removed: Prisma is no longer used after DynamoDB migration.
-// export const prisma =
-//   globalForPrisma.prisma ??
-//   new PrismaClient({
-//     log: ['query'],
-//   })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// Deprecated: This file is unused after DynamoDB migration.
